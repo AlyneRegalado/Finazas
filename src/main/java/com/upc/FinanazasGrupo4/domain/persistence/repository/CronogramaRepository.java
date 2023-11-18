e17eccdb1f1d8277d1a60cedb017c333f414a583
fiorella813valencia <u20201b944@upc.edu.pe>
2023-11-17 23:20:15 -0500

    Primer commit

---
+++ src/main/java/com/upc/FinanazasGrupo4/domain/persistence/repository/CronogramaRepository.java
@@ -0,0 +1,10 @@
+package com.upc.FinanazasGrupo4.domain.persistence.repository;
+
+import com.upc.FinanazasGrupo4.domain.model.Cronograma;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CronogramaRepository extends JpaRepository<Cronograma, Long> {
+    List<Cronograma> findAllByCustomerId(Long customerId);
+}
